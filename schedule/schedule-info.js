b92ec7389c22f3e83ef095d223e19d74af99da72
muranava <muranava@users.noreply.github.com>
2022-06-08 12:45:13 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,19 @@
+moreaEventData = [
+  {title: "E08: What are the professional codes of software engineering ethics?", url: "/aviation-topics/morea/example-ethics/experience-se-ethics-codes.html", start: "2021-07-26T23:00"},
+  {title: "E09: Doing good as a software engineer", url: "/aviation-topics/morea/example-ethics/experience-se-ethics-good.html", start: "2021-07-27T23:00"},
+  {title: "Ethics in Software Engineering", url: "/aviation-topics/modules/ethics", start: "2021-07-26", end: "2021-07-30T23:00"},
+  {title: "A03: Assess your ability to reason about ethical issues in software development", url: "/aviation-topics/morea/example-ethics/assessment-ethics.html", start: "2021-07-30T09:00"},
+  {title: "E10: Doing harm as a software engineer", url: "/aviation-topics/morea/example-ethics/experience-se-ethics-harm.html", start: "2021-07-28T23:00"},
+  {title: "Introduction to ICS 199", url: "/aviation-topics/modules/example-introduction", start: "2021-07-06", end: "2021-07-09T23:00"},
+  {title: "E03: Learn the command line", url: "/aviation-topics/morea/example-introduction/experience-learn-the-command-line.html", start: "2021-07-09T23:00"},
+  {title: "E02: Introduce yourself", url: "/aviation-topics/morea/example-introduction/experience-introduce-yourself.html", start: "2021-07-08T23:00"},
+  {title: "E01: Environment configuration", url: "/aviation-topics/morea/example-introduction/experience-environment-configuration.html", start: "2021-07-07T23:00"},
+  {title: "Open Source Software", url: "/aviation-topics/modules/example-open-source-software", start: "2021-07-19", end: "2021-07-23T23:00"},
+  {title: "A02: Assess your ability to write effectively about open source software", url: "/aviation-topics/morea/example-open-source-software/assessment-open-source-software.html", start: "2021-07-23T09:00"},
+  {title: "E07: Reflect on smart questions", url: "/aviation-topics/morea/example-open-source-software/experience-smart-questions.html", start: "2021-07-22T23:00"},
+  {title: "E06: Reflect on Javascript 1", url: "/aviation-topics/morea/example-javascript/experience-javascript-1-technical-essay.html", start: "2021-07-15T23:00"},
+  {title: "Javascript: Basics", url: "/aviation-topics/modules/example-javascript", start: "2021-07-12", end: "2021-07-16T23:00"},
+  {title: "E05: isUnique", url: "/aviation-topics/morea/example-javascript/experience-ctci.html", start: "2021-07-14T23:00"},
+  {title: "A01: Assessment of simple Javascript skills", url: "/aviation-topics/morea/example-javascript/assessment-javascript-1.html", start: "2021-07-16T09:00"},
+  {title: "E04: Introduction to Javascript with FreeCodeCamp", url: "/aviation-topics/morea/example-javascript/experience-interactive-javascript-1.html", start: "2021-07-13T23:00"}
+]
